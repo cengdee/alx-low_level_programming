c8191e53ba81f326cec3318bb46f64007a6eff8b
cengdee <acengdiana@gmail.com>
2022-09-28 04:49:07 -0700

    print rev recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,16 @@
-/*                                                                                                    
- * File: 1-print_rev_recursion.c
- * Auth: sam                                                                                                                            
- */
-
 #include "main.h"
-
 /**
- * _print_rev_recursion - Prints a string in reverse.                                                                                   
- * @s: The string to be printed.
- */                                                                                                                                     
-void _print_rev_rev_recursion(char *s)
-{                                                                                                                                       
-        if (*s)
-        {                                                                                                                               
-                _print_rev_recursion(s + 1);
-                _putchar(*s);                                                                                                           
-        }
+ * _print_rev_recursion - print a string in reverse
+ * @s: the string
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{	
+	if (!*s)
+		return;
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
